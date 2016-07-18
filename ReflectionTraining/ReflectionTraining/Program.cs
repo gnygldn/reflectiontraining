5ba527eb9565101c34efb4c14f26c20e3288176c
Güney Gülden <gnygldn@hotmail.com>
2016-07-18 18:33:29 +0300

    Add files via upload

---
+++ ReflectionTraining/ReflectionTraining/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine("Assembly Name: " + assembly.GetName().Name);
+            Console.WriteLine("Version: " + assembly.GetName().Version.ToString());
+            var assembly2 =
+                Assembly.LoadFile(
+                    "C:/Users/gnygl/Desktop/projects/currencyhelper/gnygldn.currencycalculator/gnygldn.currencycalculator/bin/Debug/gnygldn.currencycalculator.exe");
+            var types = assembly2.GetTypes();
+            foreach (var type in types)
+            {
+                var typeInfo = type.GetTypeInfo();
+                Console.WriteLine("Type " + type.FullName + " has " + typeInfo.DeclaredProperties.Count().ToString() + " properties");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
